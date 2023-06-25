ee8e70722c78f64c71505cdd854e04639421963b
AaaayushXD <94547509+AaaayushXD@users.noreply.github.com>
2023-06-25 15:02:07 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const toggle = document.querySelectorAll(".faq-toggle");
+
+toggle.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.parentNode.classList.toggle("active");
+  });
+});
